b6a98a8a0a2086ad182d6430447a499a422bfee4
hoou <tibor@mikita.eu>
2018-03-01 15:55:50 +0100

    Setup tests

---
+++ services/api/project/tests/test_api.py
@@ -0,0 +1,20 @@
+import json
+import unittest
+
+from project.tests.base import BaseTestCase
+
+
+class TestApiService(BaseTestCase):
+    """Tests for the API Service."""
+
+    def test_ping(self):
+        """Ensure the /ping route behaves correctly."""
+        response = self.client.get('/ping')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('pong!', data['message'])
+        self.assertIn('success', data['status'])
+
+
+if __name__ == '__main__':
+    unittest.main()
