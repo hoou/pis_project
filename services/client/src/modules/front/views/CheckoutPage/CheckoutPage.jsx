33e5747b5dc0251e6b938ff602f220696424b730
hoou <tibor@mikita.eu>
2018-04-21 19:32:30 +0200

    Add checkout heading

--- services/client/src/modules/front/views/CheckoutPage/CheckoutPage.jsx
+++ services/client/src/modules/front/views/CheckoutPage/CheckoutPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import HorizontalLinearStepper from "modules/front/components/Stepper/HorizontalLinearStepper/HorizontalLinearStepper";
-import {Grid} from "material-ui";
+import {Grid, Typography} from "material-ui";
 
 class CheckoutPage extends React.Component {
   render() {
     return (
       <Grid container justify="center">
         <Grid item xs={12}  md={8} lg={6}>
+          <Typography variant="display1">Checkout</Typography>
           <HorizontalLinearStepper/>
         </Grid>
       </Grid>
