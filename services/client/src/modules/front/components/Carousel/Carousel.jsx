214e8d371b85126c1fc0f2151d4febf5a2a4d2a1
hoou <tibor@mikita.eu>
2018-04-22 15:22:43 +0200

    Add photos to carousel

--- services/client/src/modules/front/components/Carousel/Carousel.jsx
+++ services/client/src/modules/front/components/Carousel/Carousel.jsx
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
+import {autoPlay} from 'react-swipeable-views-utils';
 import Pagination from './Pagination';
 import SupportTouch from './SupportTouch';
+import image1 from "modules/front/assets/img/carousel/1.jpg"
+import image2 from "modules/front/assets/img/carousel/2.jpg"
+import image3 from "modules/front/assets/img/carousel/3.jpg"
+import {withStyles} from "material-ui";
+import * as classnames from "classnames";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-const styles = {
+const styles = () => ({
   root: {
     position: 'relative',
     marginBottom: 15
   },
   slide: {
-    padding: 15,
-    minHeight: 300,
+    height: 600,
     color: '#fff',
+    overflow: "hidden",
+    position: "relative"
   },
   slide1: {
     backgroundColor: '#FEA900',
@@ -25,7 +31,14 @@ const styles = {
   slide3: {
     backgroundColor: '#6AC0FF',
   },
-};
+  slideImg: {
+    position: "absolute",
+    top: "50%",
+    width: "100%",
+    height: "auto",
+    marginTop: -550
+  }
+});
 
 class DemoAutoPlay extends Component {
   state = {
@@ -39,21 +52,28 @@ class DemoAutoPlay extends Component {
   };
 
   render() {
-    const { index } = this.state;
+    const {index} = this.state;
+    const {classes} = this.props;
 
     return (
       <SupportTouch>
-        <div style={styles.root}>
-          <AutoPlaySwipeableViews index={index} onChangeIndex={this.handleChangeIndex}>
-            <div style={Object.assign({}, styles.slide, styles.slide1)}>slide n°1</div>
-            <div style={Object.assign({}, styles.slide, styles.slide2)}>slide n°2</div>
-            <div style={Object.assign({}, styles.slide, styles.slide3)}>slide n°3</div>
+        <div className={classes.root}>
+          <AutoPlaySwipeableViews interval={5000} index={index} onChangeIndex={this.handleChangeIndex}>
+            <div className={classnames(classes.slide, classes.slide1)}>
+              <img className={classes.slideImg} src={image1}/>
+            </div>
+            <div className={classnames(classes.slide, classes.slide2)}>
+              <img className={classes.slideImg} src={image2}/>
+            </div>
+            <div className={classnames(classes.slide, classes.slide3)}>
+              <img className={classes.slideImg} src={image3}/>
+            </div>
           </AutoPlaySwipeableViews>
-          <Pagination dots={3} index={index} onChangeIndex={this.handleChangeIndex} />
+          <Pagination dots={3} index={index} onChangeIndex={this.handleChangeIndex}/>
         </div>
       </SupportTouch>
     );
   }
 }
 
-export default DemoAutoPlay;
+export default withStyles(styles)(DemoAutoPlay);
