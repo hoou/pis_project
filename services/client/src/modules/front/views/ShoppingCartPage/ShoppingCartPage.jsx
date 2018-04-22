095c1e894063e247c3eb3fe6254d99035930f833
hoou <tibor@mikita.eu>
2018-04-22 16:07:13 +0200

    When no items in cart, show info message instead of table

--- services/client/src/modules/front/views/ShoppingCartPage/ShoppingCartPage.jsx
+++ services/client/src/modules/front/views/ShoppingCartPage/ShoppingCartPage.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import {Button, Paper, withStyles} from "material-ui";
+import {Button, Paper, Typography, withStyles} from "material-ui";
 import CustomTable from "modules/front/components/ShoppingCart/CustomTable/CustomTable";
 import {Link} from "react-router-dom";
+import _ from "lodash";
 
 const styles = theme => ({
   root: {
     width: '100%',
     marginTop: theme.spacing.unit * 3,
     overflowX: 'auto',
+    minHeight: 300
   },
   checkoutButton: {
     float: "right",
     margin: 20
+  },
+  display: {
+    marginTop: 100
   }
 });
 
@@ -19,12 +24,20 @@ const ShoppingCartPage = (props) => {
   const {classes, products, items} = props;
   return (
     <Paper className={classes.root}>
-      <CustomTable products={products} items={items}/>
-      <Link to="/checkout">
-        <Button color="primary" className={classes.checkoutButton} variant="raised">
-          Checkout
-        </Button>
-      </Link>
+      {_.size(items) > 0 ? (
+        <div>
+          <CustomTable products={products} items={items}/>
+          <Link to="/checkout">
+            <Button color="primary" className={classes.checkoutButton} variant="raised">
+              Checkout
+            </Button>
+          </Link>
+        </div>
+      ) : (
+        <Typography className={classes.display} variant="display2" align="center">
+          No items in cart
+        </Typography>
+      )}
     </Paper>
   );
 };
