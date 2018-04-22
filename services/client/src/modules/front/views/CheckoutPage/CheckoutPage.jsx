0a7505702777484ed550a24d0a3071e17e818258
hoou <tibor@mikita.eu>
2018-04-22 16:21:50 +0200

    Redirect user to homepage when shopping cart is empty and user is trying to checkout

--- services/client/src/modules/front/views/CheckoutPage/CheckoutPage.jsx
+++ services/client/src/modules/front/views/CheckoutPage/CheckoutPage.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import HorizontalLinearStepper from "modules/front/components/Stepper/HorizontalLinearStepper/HorizontalLinearStepper";
 import {Grid, Typography} from "material-ui";
+import _ from "lodash"
+import {Redirect} from "react-router-dom";
 
 const CheckoutPage = (props) => {
   const {products, cartItems} = props;
   return (
-    <Grid container justify="center">
-      <Grid item xs={12} md={8} lg={6}>
-        <Typography variant="display1">Checkout</Typography>
-        <HorizontalLinearStepper products={products} cartItems={cartItems}/>
+    _.size(cartItems) > 0 ?
+      <Grid container justify="center">
+        <Grid item xs={12} md={8} lg={6}>
+          <Typography variant="display1">Checkout</Typography>
+          <HorizontalLinearStepper products={products} cartItems={cartItems}/>
+        </Grid>
       </Grid>
-    </Grid>
+      :
+      <Redirect to="/home"/>
   )
 };
 
